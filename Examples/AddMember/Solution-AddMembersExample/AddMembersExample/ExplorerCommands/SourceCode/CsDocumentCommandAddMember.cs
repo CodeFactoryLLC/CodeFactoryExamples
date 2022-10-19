cbf55dbf6c35965b1912ebdb3ade22863c9a7278
Stephen Giffin <stephen.giffin@lifecycledelivery.com>
2022-10-18 17:16:45 -0700

    Initial command with method implemented.

---
+++ Examples/AddMember/Solution-AddMembersExample/AddMembersExample/ExplorerCommands/SourceCode/CsDocumentCommandAddMember.cs
@@ -0,0 +1,135 @@
+﻿using CodeFactory.DotNet.CSharp;
+using CodeFactory.Logging;
+using CodeFactory.VisualStudio;
+using CodeFactory.VisualStudio.SolutionExplorer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddMembersExample.Logic;
+using CodeFactory;
+
+namespace AddMembersExample.ExplorerCommands.SourceCode
+{
+    /// <summary>
+    /// Code factory command for automation of a C# document when selected from a project in solution explorer.
+    /// </summary>
+    public class CsDocumentCommandAddMember : CSharpSourceCommandBase
+    {
+        private static readonly string commandTitle = "Add Member";
+        private static readonly string commandDescription = "Adds interface members that are missing from the target class that implements the interface.";
+
+#pragma warning disable CS1998
+
+        /// <inheritdoc />
+        public CsDocumentCommandAddMember(ILogger logger, IVsActions vsActions) : base(logger, vsActions, commandTitle, commandDescription)
+        {
+            //Intentionally blank
+        }
+
+        #region Overrides of VsCommandBase<IVsCSharpDocument>
+
+        /// <summary>
+        /// Validation logic that will determine if this command should be enabled for execution.
+        /// </summary>
+        /// <param name="result">The target model data that will be used to determine if this command should be enabled.</param>
+        /// <returns>Boolean flag that will tell code factory to enable this command or disable it.</returns>
+        public override async Task<bool> EnableCommandAsync(VsCSharpSource result)
+        {
+            //Result that determines if the the command is enabled and visible in the context menu for execution.
+            bool isEnabled = false;
+
+            try
+            {
+                //Getting the hosting class from the source code file.
+                var classData = result.SourceCode.Classes.FirstOrDefault();
+
+                //Determine if a class was found. If not do not enable the command.
+                isEnabled = classData != null;
+
+                //If isEnabled check to make sure there are missing interface members. Otherwise do not display the command.
+                if(isEnabled) isEnabled = classData.MissingInterfaceMembers().Any();
+
+            }
+            catch (Exception unhandledError)
+            {
+                _logger.Error($"The following unhandled error occured while checking if the solution explorer C# document command {commandTitle} is enabled. ",
+                    unhandledError);
+                isEnabled = false;
+            }
+
+            return isEnabled;
+        }
+
+        /// <summary>
+        /// Code factory framework calls this method when the command has been executed. 
+        /// </summary>
+        /// <param name="result">The code factory model that has generated and provided to the command to process.</param>
+        public override async Task ExecuteCommandAsync(VsCSharpSource result)
+        {
+            try
+            {
+                //Local variable to keep track of the source code in the file.
+                var sourceCode = result.SourceCode;
+
+                //Getting the implementation class in the source code.
+                var classData = sourceCode?.Classes.FirstOrDefault();
+
+                //If not class is found exit the command.
+                if(classData == null) return;
+
+                //Getting the missing interface members
+                var missingMembers = classData.MissingInterfaceMembers();
+
+                //If no members are missing then return nothing to automate.
+                if(!missingMembers.Any()) return;
+
+                
+                //Loop through each missing member and add it to the class.
+                foreach (var missingMember in missingMembers)
+                {
+                    //Switching on the type of member that is missing
+                    switch (missingMember.MemberType)
+                    {
+                        case CsMemberType.Event:
+                            break;
+
+                        case CsMemberType.Method:
+
+                            var methodData = missingMember as CsMethod;
+
+                            var formattedSource = methodData.FormatMethodSyntax();
+
+                            if(string.IsNullOrEmpty(formattedSource)) continue;
+
+                            var formatter = new SourceFormatter();
+                            formatter.AppendCodeBlock(2,formattedSource);
+
+                            sourceCode = await classData.AddToEndAsync(formatter.ReturnSource());
+
+                            classData = sourceCode.GetModel(classData.LookupPath) as CsClass;
+
+                            break;
+
+                        case CsMemberType.Property:
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+            }
+            catch (Exception unhandledError)
+            {
+                _logger.Error($"The following unhandled error occured while executing the solution explorer C# document command {commandTitle}. ",
+                    unhandledError);
+
+            }
+
+        }
+
+        #endregion
+    }
+}
---
+++ Examples/AddMember/Solution-AddMembersExample/AddMembersExample/Logic/MemberFormatExtensions.cs
@@ -0,0 +1,116 @@
+﻿using CodeFactory;
+using CodeFactory.DotNet.CSharp;
+using CodeFactory.Formatting.CSharp;
+namespace AddMembersExample.Logic
+{
+    /// <summary>
+    /// Extension methods that help format member models into C# implementation syntax.
+    /// </summary>
+    internal static class MemberFormatExtensions
+    {
+        /// <summary>
+        /// Formats a method with logging, bounds checking, and error handling managed in a try catch block
+        /// </summary>
+        /// <param name="source">The source method model to generate the source </param>
+        /// <returns>The fully formatted syntax for the method</returns>
+        public static string FormatMethodSyntax(this CsMethod source)
+        { 
+            //If no source is found then we return an empty method format.
+            if(source == null) return null;
+            
+            //The source formatter formats the source code that will be emitted to the class.
+            SourceFormatter formatter = new SourceFormatter();
+
+            //Checking if the method has xml documentation.
+            if(source.HasDocumentation)
+            {
+                //Iterate over all the xml docs
+                foreach (var xmlDoc in source.CSharpFormatXmlDocumentationEnumerator())
+                {
+                    //Write the docs to the formatter.
+                    formatter.AppendCodeLine(0,xmlDoc);
+                }        
+            }
+
+            //If the method has attributes asigned to make sure they get included in the signature. 
+            if(source.HasAttributes)
+            {
+                //Iterate over the attributes
+                foreach (var attributeData in source.Attributes)
+                {
+                    //Get the C# attribute for each attribute and append it to the formatter output.
+                    formatter.AppendCodeLine(0,attributeData.CSharpFormatAttributeSignature());
+                }    
+            }
+
+            //Generate the method signature and add it to the formatter.
+            formatter.AppendCodeLine(0,source.CSharpFormatStandardMethodSignatureWithAsync());
+
+            //Adding the statement start for the method.
+            formatter.AppendCodeLine(0,"{");
+
+            //Adding entry point logging for the method.
+            formatter.AppendCodeLine(1,$"_logger.LogInformation(\"Entering Method {source.Name}\");");
+            formatter.AppendCodeLine(1);
+
+            //Checking the method to see if has parameters. If so bounds check the parameters.
+            if(source.HasParameters)
+            {
+                foreach (var parmData in source.Parameters)
+                {   
+                    //If the parameter has a default value then we dont bounds check it.
+                    if(parmData.HasDefaultValue) continue;
+
+                    //If the parameter is a value type then we dont bounds check it.
+                    if(parmData.ParameterType.IsValueType) continue;
+
+                    //If the paramter is a string type then bounds check for empty or null
+                    if(parmData.ParameterType.IsWellKnownType & parmData.ParameterType.WellKnownType == CsKnownLanguageType.String)
+                    {
+                        formatter.AppendCodeLine(2,$"if(string.IsNullOrEmpty({parmData.Name}))");
+                        formatter.AppendCodeLine(2,"{");
+                        formatter.AppendCodeLine(3,$"_logger.LogError($\"The parameter '{{nameof({parmData.Name})}}' was not provided raising \");");
+                        formatter.AppendCodeLine(3,$"_logger.LogInformation(\"Exiting Method {source.Name}\")");
+                        formatter.AppendCodeLine(3,$"throw new ArgumentException(\"The required argument was missing data\",nameof({parmData.Name}));");
+                        formatter.AppendCodeLine(2,"}");
+                        formatter.AppendCodeLine(2);
+                    }
+                    //If the parameter is not a string then check for null condition.
+                    else
+                    { 
+                        formatter.AppendCodeLine(2,$"if({parmData.Name} == null)");
+                        formatter.AppendCodeLine(2,"{");
+                        formatter.AppendCodeLine(3,$"_logger.LogError($\"The parameter '{{nameof({parmData.Name})}}' was not provided raising \");");
+                        formatter.AppendCodeLine(3,$"_logger.LogInformation(\"Exiting Method {source.Name}\");");
+                        formatter.AppendCodeLine(3,$"throw new ArgumentNullException(\"The required argument was missing data\",nameof({parmData.Name}));");
+                        formatter.AppendCodeLine(2,"}");
+                        formatter.AppendCodeLine(2);
+                    }
+                }
+            }
+
+            //Adding try and catch blocks for the method.
+            formatter.AppendCodeLine(1,"try");
+            formatter.AppendCodeLine(1,"{");
+            formatter.AppendCodeLine(1);
+            formatter.AppendCodeLine(1,"}");
+            formatter.AppendCodeLine(1,"catch (Exception unhandledError)");
+            formatter.AppendCodeLine(1,"{");
+            formatter.AppendCodeLine(2,$"_logger.LogError(unhandledError, $\"An unhandled error occured in {source.Name}, see exception for details.\");");
+            formatter.AppendCodeLine(2,$"_logger.LogInformation(\"Exiting Method {source.Name}\");");
+            formatter.AppendCodeLine(2,"throw unhandledError;");
+            formatter.AppendCodeLine(1,"}");
+            formatter.AppendCodeLine(1);
+
+            formatter.AppendCodeLine(1,$"_logger.LogInformation(\"Exiting Method {source.Name}\");");
+            formatter.AppendCodeLine(1);
+
+            //Adding the statement end for the method.
+            formatter.AppendCodeLine(0,"}");
+            formatter.AppendCodeLine(0);
+
+            //Returning the full formatted source code.
+            return formatter.ReturnSource();
+        }
+    }
+}
